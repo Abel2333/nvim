eb7783c00286945ef109673f8c8dc9f7a711be21
Abel2333 <abelsparda@outlook.com>
2024-12-29 22:27:37 +0800

    Fixes clang-extension bug
    comment cmp part

--- lua/abel/plugins/lsp/clangd_extensions.lua
+++ lua/abel/plugins/lsp/clangd_extensions.lua
@@ -21,29 +21,29 @@ return {
 
         local group = vim.api.nvim_create_augroup('clangd_extensions', { clear = true })
 
-        vim.api.nvim_create_autocmd('Filetype', {
-            group = group,
-            desc = 'Setup clangd_extensions scores for cmp',
-            pattern = 'c,cpp,cxx',
-            callback = function()
-                local cmp = require 'cmp'
-                cmp.setup.buffer {
-                    ---@diagnostic disable-next-line: missing-fields
-                    sorting = {
-                        comparators = {
-                            cmp.config.compare.offset,
-                            cmp.config.compare.exact,
-                            cmp.config.compare.recently_used,
-                            require 'clangd_extensions.cmp_scores',
-                            cmp.config.compare.kind,
-                            cmp.config.compare.sort_text,
-                            cmp.config.compare.length,
-                            cmp.config.compare.order,
-                        },
-                    },
-                }
-            end,
-        })
+        -- vim.api.nvim_create_autocmd('Filetype', {
+        --     group = group,
+        --     desc = 'Setup clangd_extensions scores for cmp',
+        --     pattern = 'c,cpp,cxx',
+        --     callback = function()
+        --         local cmp = require 'cmp'
+        --         cmp.setup.buffer {
+        --             ---@diagnostic disable-next-line: missing-fields
+        --             sorting = {
+        --                 comparators = {
+        --                     cmp.config.compare.offset,
+        --                     cmp.config.compare.exact,
+        --                     cmp.config.compare.recently_used,
+        --                     require 'clangd_extensions.cmp_scores',
+        --                     cmp.config.compare.kind,
+        --                     cmp.config.compare.sort_text,
+        --                     cmp.config.compare.length,
+        --                     cmp.config.compare.order,
+        --                 },
+        --             },
+        --         }
+        --     end,
+        -- })
 
         vim.api.nvim_create_autocmd('LspAttach', {
             group = group,
