6e01f77d54d374014b9431102ed9bb9449f1fe5f
Abel2333 <abelsparda@outlook.com>
2025-01-07 09:26:58 +0800

    Update blink configuration

--- lua/abel/plugins/editor/blink.lua
+++ lua/abel/plugins/editor/blink.lua
@@ -64,9 +64,14 @@ return {
         completion = {
             ghost_text = { enabled = true },
             list = {
-                selection = function(ctx)
-                    return ctx.mode == 'cmdline' and 'auto_insert' or 'preselect'
-                end,
+                selection = {
+                    preselect = function(ctx)
+                        return ctx.mode ~= 'cmdline' and not require('blink.cmp').snippet_active { direction = 1 }
+                    end,
+                    auto_insert = function(ctx)
+                        return ctx.mode ~= 'cmdline'
+                    end,
+                },
             },
             menu = {
                 border = 'rounded',
